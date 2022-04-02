b4faf795d0088716be1b9da3e9960e4f12206a7e
trevis <trevismorvany@gmail.com>
2022-04-02 15:15:57 +0200

    Arret du client simple

--- src/cli.cpp
+++ src/cli.cpp
@@ -1,16 +1,35 @@
+#include "client.hpp"
 #include <fmt/color.h>
 #include <fmt/core.h>
 #include <iostream>
 #include <string>
 #include <thread>
 
-#include "client.hpp"
+#include <poll.h>
+#include <sys/eventfd.h>
+#include <unistd.h>
+
+// permet de notifier le thread qui attend l'input sur stdin
+int event_fd = eventfd(0, 0);
+
+// bloque jusqu'a ce qu'il y ait des données dispo sur stdin
+// renvoie true si des données sont dispo, false si event_fd a ete notifie
+bool wait_stdin() {
+    static pollfd pfds[2] = {
+        {fileno(stdin), POLLIN, 0},
+        {event_fd, POLLIN, 0}
+    };
+
+    poll(pfds, 2, -1);
+
+    return pfds[0].revents & POLLIN;
+}
 
 void send_loop(Client& client) {
     std::string message;
     ssize_t bytes_written = 0;
 
-    while (bytes_written != -1) {
+    while (bytes_written != -1 && wait_stdin()) {
         std::getline(std::cin, message);
         auto sep = message.find_first_of(' ');
         if (message == "stop") {
@@ -59,8 +78,10 @@ int main(int argc, char** argv) {
         fmt::print("{}\n", message);
     }
 
+    write(event_fd, "00000000", 8);
     thr.join();
     client.disconnect();
+    close(event_fd);
 
     return 0;
 }
